816581bd74dd8a666394a6ffe8673f7fdf934bf6
WhyK <windchimeyk@gmail.com>
2021-08-12 01:37:22 +0900

    feat: add encrypt sample

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate aes_gcm_siv;
+
+use aes_gcm_siv::aead::{Aead, NewAead};
+use aes_gcm_siv::{Aes256GcmSiv, Key, Nonce}; // Or `Aes128GcmSiv`
+
 fn main() {
-    println!("Hello, world!");
+    let key = Key::from_slice(b"an example very very secret key.");
+    let cipher = Aes256GcmSiv::new(key);
+    let nonce = Nonce::from_slice(b"unique nonce"); // 96-bits; unique per message
+
+    let ciphertext = cipher
+        .encrypt(nonce, b"plaintext message".as_ref())
+        .expect("encryption failure!"); // NOTE: handle this error to avoid panics!
+
+    println!("{:?}", ciphertext);
+
+    let plaintext = cipher
+        .decrypt(nonce, ciphertext.as_ref())
+        .expect("decryption failure!"); // NOTE: handle this error to avoid panics!
+
+    println!("{}", String::from_utf8(plaintext.to_vec()).unwrap());
+    assert_eq!(&plaintext, b"plaintext message");
 }
