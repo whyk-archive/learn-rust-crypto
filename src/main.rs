fbe73b67c144ea9f9720bc45e1765826abdcaed1
WhyK <windchimeyk@gmail.com>
2021-08-12 22:15:10 +0900

    feat: return value is String

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate aes_gcm_siv;
 use aes_gcm_siv::aead::{Aead, NewAead};
 use aes_gcm_siv::{Aes256GcmSiv, Key, Nonce}; // Or `Aes128GcmSiv`
 
-fn crypto() -> Vec<u8> {
+fn crypto() -> String {
     let key = Key::from_slice(b"an example very very secret key.");
     let cipher = Aes256GcmSiv::new(key);
     let nonce = Nonce::from_slice(b"unique nonce"); // 96-bits; unique per message
@@ -18,12 +18,12 @@ fn crypto() -> Vec<u8> {
         .decrypt(nonce, ciphertext.as_ref())
         .expect("decryption failure!"); // NOTE: handle this error to avoid panics!
 
-    plaintext
+    String::from_utf8(plaintext.to_vec()).unwrap()
 }
 
 fn main() {
     let plaintext = crypto();
 
-    println!("{}", String::from_utf8(plaintext.to_vec()).unwrap());
-    assert_eq!(&plaintext, b"plaintext message");
+    println!("{}", plaintext);
+    assert_eq!(&plaintext, "plaintext message");
 }
