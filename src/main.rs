e32eac5aa561656b7a524560aca6934aa7a556de
WhyK <windchimeyk@gmail.com>
2021-08-12 22:05:59 +0900

    refactor: slice crypto function

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate aes_gcm_siv;
 use aes_gcm_siv::aead::{Aead, NewAead};
 use aes_gcm_siv::{Aes256GcmSiv, Key, Nonce}; // Or `Aes128GcmSiv`
 
-fn main() {
+fn crypto() -> Vec<u8> {
     let key = Key::from_slice(b"an example very very secret key.");
     let cipher = Aes256GcmSiv::new(key);
     let nonce = Nonce::from_slice(b"unique nonce"); // 96-bits; unique per message
@@ -17,6 +17,12 @@ fn main() {
     let plaintext = cipher
         .decrypt(nonce, ciphertext.as_ref())
         .expect("decryption failure!"); // NOTE: handle this error to avoid panics!
+
+    plaintext
+}
+
+fn main() {
+    let plaintext = crypto();
 
     println!("{}", String::from_utf8(plaintext.to_vec()).unwrap());
     assert_eq!(&plaintext, b"plaintext message");
